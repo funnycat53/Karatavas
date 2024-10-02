650e6c0157e2a3e77df0032c3220c19378ba70b8
funnycat53 <patriks.jkb@gmail.com>
2024-10-02 15:20:22 +0300

    real

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+*Spēles pārskats*
+
+1. Mērķis - Uzminēt doto vārdu, ievadot burtus. 
+2. Spēles gaita - Spēlētājs izvēlas vienu burtu ko minēt. Ja dotajā vārda ir tas burts, tad tas parādīsies, ja nav, 
+tad tas burts neparādīsies un spēlētājam minējumu skaits samazināsies.
+3. Uzvaras nosacījumi - Spēlētājs uzvar, ja uzmin doto vārdu.
+4. Zaudēšanas nosacījumi - Spēlētājs zaudē, ja neuzmin doto vārdu nosacītajā minējumu skaitā.
+
+*Komponentes*
+
+1. Vārdu saraksts, izvēle - Viss saraksts ar vārdiem atrodas failā vardu_saraksts.py
+"""
